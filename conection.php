67da5579f5480bb7bef635075c0c88595dda5c72
CSteigueber <c.steigueber@gmail.com>
2020-01-02 10:34:36 +0100

    initial commit

---
+++ conection.php
@@ -0,0 +1,17 @@
+<?php
+function openConnection() {
+ // Try to figure out what these should be for you
+ $dbhost    = "localhost";
+ $dbuser    = "root";
+ $dbpass    = "root";
+ $db        = "becode";
+ 
+ // Try to understand what happens here 
+$pdo = new PDO('mysql:host='. $dbhost .';dbname='. $db, $dbuser, $dbpass);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+ 
+ // Why we do this here
+ return $pdo;
+}
+echo "Howdie partner!";
