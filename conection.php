b19dca37709509e696e8e6d4b39c602a19a26e33
CSteigueber <c.steigueber@gmail.com>
2020-01-02 15:46:54 +0100

    going somewhere

--- conection.php
+++ conection.php
@@ -7,10 +7,13 @@ function openConnection() {
  $db        = "becode";
  
  // Try to understand what happens here 
-$pdo = new PDO('mysql:host='. $dbhost .';dbname='. $db, $dbuser, $dbpass);
+ echo "before";
+ $pdo = new PDO('mysql:host='. $dbhost .';dbname='. $db, $dbuser, $dbpass);
+echo "after";
+//$pdo->query("INSERT INTO students (first_name) VALUES ('Stijn')"); 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
- 
+
  // Why we do this here
  return $pdo;
 }
--- index.php
+++ index.php
@@ -15,10 +15,12 @@ function check_post(){
 
 require 'conection.php';
 if (check_post()){
-    echo "nice";
-    if (openConnection()){
+    echo 'got it';
+    $pdo=openConnection();
+    if ($pdo){
         $sql= "INSERT INTO students (first_name, last_name, username, gender, linkedin, github, email, preferred_language, avatar, video, quote, quote_author";
-        
+        $sql.=" VALUES (".$_POST["first_name"].",".$_POST["last_name"].",".$_POST["username"].",".$_POST["gender"][0].",".$_POST["linkedin"].",".$_POST["github"].",".$_POST["email"].",".$_POST["language"][0].",".$_POST["avatar"].",".$_POST["video"].",".$_POST["quote"].",".$_POST["quote_author"];
+        echo $sql;
     }
     else{
         echo "Connection failed!";
